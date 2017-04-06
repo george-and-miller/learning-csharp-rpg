8ca441ac154264660c06614f86cbb0d53b625150
George Miller <george.and.miller@gmail.com>
2017-04-05 22:33:01 -0400

    Almost done with the changes to game display logic, on page 7/9 of the source copy. 1:41 hours left in the flight.

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider _generator =
+            new RNGCryptoServiceProvider();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOFRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // To deal with rounding issues.
+            double multiplier = Math.Max(0, (asciiValueOFRandomCharacter / 255) - 0.00000000001d);
+
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+}
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -15,19 +15,217 @@ namespace SuperAdventure
     public partial class SuperAdventure : Form
     {
         private Player _player;
+        private Monster _currentMonster;
 
         public SuperAdventure()
         {
             InitializeComponent();
 
-            _player = new Player(10, 10, 20, 0 , 1);
-
-            Location location = new Location(1, "Home", "This is your house.");
+            _player = new Player(10, 10, 20, 0, 1);
+            MoveTo(World.LocationById(World.LOCATION_ID_HOME));
+            _player.Inventory.Add(new InventoryItem(
+                World.ItemById(World.ITEM_ID_RUSTY_SWORD), 1));
 
             lblHitPoints.Text = _player.CurrentHitPoints.ToString();
             lblGold.Text = _player.Gold.ToString();
             lblExperience.Text = _player.ExperiencePoints.ToString();
             lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void MoveTo(Location newLocation)
+        {
+            if (newLocation.ItemRequiredToEnter != null)
+            {
+                bool playerHasRequiredItem = false;
+
+                foreach (InventoryItem ii in _player.Inventory)
+                {
+                    if (ii.Details.ID == newLocation.ItemRequiredToEnter.ID)
+                    {
+                        playerHasRequiredItem = true;
+                        break;
+                    }
+                }
+
+                if (!playerHasRequiredItem)
+                {
+                    rtbMessages.Text += "You must have a " +
+                        newLocation.ItemRequiredToEnter.Name +
+                        " to enter this location." + Environment.NewLine;
+                    return;
+                }
+            }
+
+            _player.CurrentLocation = newLocation;
+
+            btnNorth.Visible = (newLocation.LocationToNorth != null);
+            btnEast.Visible = (newLocation.LocationToEast != null);
+            btnWest.Visible = (newLocation.LocationToWest != null);
+            btnSouth.Visible = (newLocation.LocationToSouth != null);
+
+            rtbLocation.Text = newLocation.Name + Environment.NewLine;
+            rtbLocation.Text += newLocation.Description + Environment.NewLine;
+
+            _player.CurrentHitPoints = _player.MaximumHitPoints;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+
+            if (newLocation.QuestAvailableHere != null)
+            {
+                bool playerAlreadyHasQuest = false;
+                bool playerAlreadyCompletedQuest = false;
+
+                foreach (PlayerQuest playerQuest in _player.Quests)
+                {
+                    if (playerQuest.Details.ID == newLocation.QuestAvailableHere.ID)
+                    {
+                        playerAlreadyHasQuest = true;
+
+                        if (playerQuest.IsCompleted)
+                        {
+                            playerAlreadyCompletedQuest = true;
+                        }
+                    }
+                }
+
+                if (playerAlreadyHasQuest)
+                {
+                    if (!playerAlreadyCompletedQuest)
+                    {
+                        bool playerHasAllItemsToCompleteQuest = true;
+                        foreach (QuestCompletionItem qci in
+                            newLocation.QuestAvailableHere.QuestCompletionItems)
+                        {
+                            bool foundItemInPlayersInventory = false;
+
+                            foreach (InventoryItem ii in _player.Inventory)
+                            {
+                                if (ii.Details.ID == qci.Details.ID)
+                                {
+                                    foundItemInPlayersInventory = true;
+
+                                    if (ii.Quantity < qci.Quantity)
+                                    {
+                                        playerHasAllItemsToCompleteQuest = false;
+                                        break;
+                                    }
+
+                                    break;
+                                }
+                            }
+
+                            if (!foundItemInPlayersInventory)
+                            {
+                                playerHasAllItemsToCompleteQuest = false;
+                                break;
+                            }
+                        }
+
+                        if (playerHasAllItemsToCompleteQuest)
+                        {
+                            rtbMessages.Text += Environment.NewLine;
+                            rtbMessages.Text += "You complete the " +
+                                newLocation.QuestAvailableHere.Name +
+                                " quest." + Environment.NewLine;
+
+                            foreach (QuestCompletionItem qci in
+                                newLocation.QuestAvailableHere.QuestCompletionItems)
+                            {
+                                foreach (InventoryItem ii in _player.Inventory)
+                                {
+                                    if (ii.Details.ID == qci.Details.ID)
+                                    {
+                                        ii.Quantity -= qci.Quantity;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            rtbMessages.Text += "You recieve: " + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardExperiencePoints.ToString() +
+                                " experience points " + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardGold.ToString() +
+                                " gold " + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardItem.Name +
+                                Environment.NewLine;
+                            rtbMessages.Text += Environment.NewLine;
+
+                            _player.ExperiencePoints +=
+                                newLocation.QuestAvailableHere.RewardExperiencePoints;
+                            _player.Gold +=
+                                newLocation.QuestAvailableHere.RewardGold;
+
+                            bool addedItemToPlayerInventory = false;
+
+                            foreach (InventoryItem ii in _player.Inventory)
+                            {
+                                if (ii.Details.ID ==
+                                    newLocation.QuestAvailableHere.RewardItem.ID)
+                                {
+                                    ii.Quantity++;
+
+                                    addedItemToPlayerInventory = true;
+
+                                    break;
+                                }
+                            }
+
+                            if (!addedItemToPlayerInventory)
+                            {
+                                _player.Inventory.Add(new InventoryItem(
+                                    newLocation.QuestAvailableHere.RewardItem, 1));
+                            }
+
+                            foreach (PlayerQuest pq in _player.Quests)
+                            {
+                                if (pq.Details.ID == newLocation.QuestAvailableHere.ID)
+                                {
+                                    pq.IsCompleted = true;
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else
+                {
+                    rtbMessages.Text += "You recieve the " +
+                        newLocation.QuestAvailableHere.Name +
+                        " quest " + Environment.NewLine;
+                    rtbMessages.Text += newLocation.QuestAvailableHere.Description +
+                        Environment.NewLine;
+                    rtbMessages.Text += "To complete it, return with: " +
+                        Environment.NewLine;
+                    foreach (QuestCompletionItem qci in
+                        newLocation.QuestAvailableHere.QuestCompletionItems)
+                    {
+                        if (qci.Quantity == 1)
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " +
+                                qci.Details.Name + Environment.NewLine;
+                        }
+                        else
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " +
+                                    qci.Details.Name + Environment.NewLine;
+                        }
+                    }
+
+                    rtbMessages.Text += Environment.NewLine;
+
+                    _player.Quests.Add(new PlayerQuest(newLocation.QuestAvailableHere));
+                }
+            }
+
+            if (newLocation.MonsterLivingHere != null)
+            {
+
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -37,21 +235,23 @@ namespace SuperAdventure
 
         private void btnNorth_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToNorth);
         }
 
         private void btnWest_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToWest);
         }
 
         private void btnEast_Click(object sender, EventArgs e)
         {
+            MoveTo(_player.CurrentLocation.LocationToEast);
 
         }
 
         private void btnSouth_Click(object sender, EventArgs e)
         {
+            MoveTo(_player.CurrentLocation.LocationToSouth);
 
         }
 
